c7a934e8c655b5004eaa07b87d3b1e34efef40d6
Txgyfss <3266572873@qq.com>
2025-05-19 06:47:41 +0800

    message 13: refactor sleep()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,7 @@ import time
 import unittest
 from selenium.webdriver.common.by import By
 from django.test import LiveServerTestCase
+max_wait=10
 class NewVisitorTest(LiveServerTestCase):
   
   def setUp(self):
@@ -11,6 +12,18 @@ class NewVisitorTest(LiveServerTestCase):
     
   def tearDown(self):
     self.browser.quit()
+  def wait_for_row_in_list_table(self,row_text):
+    start_time=time.time()
+    while True:
+      try:
+        table=self.browser.find_element(By.ID,'id_list_table')
+        rows=table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn(row_text,[row.text for row in rows])
+        return
+      except (AssertionError,WebDriverException) as e:
+        if time.time()-start_time>max_wait:
+          raise e
+        time.sleep(0.5)
   def check_for_row_in_list_table(self,row_text):
     table=self.browser.find_element(By.ID,'id_list_table')
     rows=table.find_elements(By.TAG_NAME,'tr')
@@ -42,7 +55,7 @@ class NewVisitorTest(LiveServerTestCase):
     inputbox=self.browser.find_element(By.ID,'id_new_item')
     inputbox.send_keys('gift to girlfriend')
     inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
+
 
     # 页面再次更新，她的清单中显示了这两个待办事项
     self.check_for_row_in_list_table('1:Buy flowers')
