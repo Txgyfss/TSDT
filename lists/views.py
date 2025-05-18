9a1afb7b774b7c35d45e02803f53a1b3a0d7232e
Txgyfss <3266572873@qq.com>
2025-05-19 06:33:11 +0800

    message 11: Show all items in template

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from lists.models import Item
 def home_page(request):
   if request.method=='POST':
-    return HttpResponse(request.POST['item_text'])
-  return render(request,'home.html')
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/')
+  items=Item.objects.all()
+  return render(request,'home.html',{'items':items})
 # Create your views here.
