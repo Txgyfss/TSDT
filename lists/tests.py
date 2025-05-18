1000bd9087654f50e5530fb7e39f0fbfa61ca995
Txgyfss <3266572873@qq.com>
2025-05-19 05:42:56 +0800

    message 9: After refactoring the functional_tests

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 class HomePageTest(TestCase):
   def test_root_url_resolves_to_home_page_view(self):
     found=resolve('/')
@@ -10,4 +11,7 @@ class HomePageTest(TestCase):
     def test_use_home_template(self):
       response=self.client.get('/')
       self.assertTemplateUsed(response,'home.html')
- 
+    def test_can_save_a_POST_request(self):
+      response=self.client.post('/',data={'item_text':'A new list item'})
+      self.assertIn('A new list item',response.content.decode())
+      self.assertTemplateUsed(response,'home.html')
